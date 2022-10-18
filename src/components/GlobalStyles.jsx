2f76f07641441477dee38b0ff6229ab5844305b6
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-10-18 14:18:05 +0200

    Initialize GlobalStyles

---
+++ src/components/GlobalStyles.jsx
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        font-size: 1rem;
+        box-sizing: border-box;
+    }
+
+    body {
+        max-width: 100vw;
+        overflow-x: hidden;
+    }
+`;
