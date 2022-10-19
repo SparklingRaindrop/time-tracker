83603177f4e3a78ecfe5c81172dca7e713eec494
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-10-19 15:17:27 +0200

    Init ModalCreateProject

---
+++ src/routers/Projects/ModalCreateProject/ModalCreateProject.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import { Button } from '../../../components';
+import { Wrapper, Title, Input, Label, Colors, ColorSwatch, CloseButton, Header } from '../../../blocks'
+
+const test = {
+    title: 'Create new project'
+}
+
+const colors = [
+    '#FF80ED', '#00FFFF', '#FF7373', '#FFD700', '#008080',
+    '#BADA55', '#66CDAA', '#FAEBD7', '#C6E2FF', '#8A2BE2'
+];
+
+export default function ModalCreateProject(props) {
+    const { onClose } = props;
+    const { title } = test;
+
+    return (
+        <Wrapper>
+            <Header>
+                <Title>{title}</Title>
+                <CloseButton onClick={onClose} name='close' />
+            </Header>
+            <div>
+                <Label>project name</Label>
+                <Input placeholder='project name' />
+            </div>
+            <Colors>
+                {colors.map(color => <ColorSwatch key={color} $color={color} />)}
+            </Colors>
+            <Button label='Create new project' onClick={() => console.log('Create new project')} />
+        </Wrapper>
+    )
+}
+
+ModalCreateProject.propTypes = {
+    onClose: PropTypes.func.isRequired,
+}
