3b38259abc59e724f49274fda66d9777960becd7
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-10-19 15:18:51 +0200

    Init StopWatch

---
+++ src/routers/TimeKeeper/blocks/StopWatch/StopWatch.jsx
@@ -0,0 +1,11 @@
+import { Time, Wrapper } from './styled';
+
+export default function StopWatch() {
+    return (
+        <Wrapper>
+            <Time>
+                00:00:00
+            </Time>
+        </Wrapper>
+    )
+}
