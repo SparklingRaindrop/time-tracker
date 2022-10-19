43242c2aa42c098e0cdd52c0cacbe1a239e8de78
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-10-19 15:20:15 +0200

    Update components export

--- src/components/index.js
+++ src/components/index.js
@@ -2,9 +2,19 @@ import Icon from './Icon';
 import { GlobalStyles } from './GlobalStyles';
 import Nav from './Nav';
 import Main from './Main';
+import Wrapper from './Wrapper';
+import {List, ListItem} from './ListView';
+import Button from './Button';
+import IconButton from './IconButton';
+
 export {
     Icon,
     GlobalStyles,
     Nav,
-    Main
+    Main,
+    Wrapper,
+    List,
+    ListItem,
+    Button,
+    IconButton
 }
