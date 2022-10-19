41767bc79b017e3297063762898e9f43a4d3c4dc
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-10-19 15:11:55 +0200

    Fix overflow nad padding on Main

--- src/components/Main/styled.js
+++ src/components/Main/styled.js
@@ -3,10 +3,8 @@ import styled from 'styled-components';
 export const Wrapper = styled.main`
     width: 100vw;
     height: 100%;
-    padding: 0 1rem;
 
-    overflow-y: auto;
-    overflow-x: hidden;
+    overflow: hidden;
 
     position: relative;
 
