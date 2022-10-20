c8d81d8339a1461bd6102e8ebf2598b8cc62fafd
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-10-20 08:13:01 +0200

    Add logic to check login status and redirect

--- src/routers/Layout/Layout.jsx
+++ src/routers/Layout/Layout.jsx
@@ -1,11 +1,32 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import { ContentWrapper } from './styled';
 import NavBar from './NavBar';
 import TopBar from './TopBar';
+import { useContext, useEffect } from 'react';
+import { UserDataContext } from '../../context/UserDataProvider';
 
 export default function Layout() {
+    const { pathname } = useLocation();
+    const { userId, getProjects, setProjects } = useContext(UserDataContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        async function fetchProjects() {
+            const { status, data } = await getProjects(userId);
+            if (status === 200) {
+                setProjects(data);
+            }
+        }
+
+        if (!userId) {
+            navigate('/');
+            return;
+        }
+        fetchProjects();
+    }, [userId]);
+
     return (
-        <ContentWrapper>
+        <ContentWrapper $noTopBar={pathname === '/'}>
             <TopBar />
             <Outlet />
             <NavBar />
--- src/routers/Layout/styled.js
+++ src/routers/Layout/styled.js
@@ -3,5 +3,5 @@ import styled from 'styled-components';
 export const ContentWrapper = styled.div`
     height: 100vh;
     display: grid;
-    grid-template-rows: max-content 1fr max-content;
+    grid-template-rows: ${({$noTopBar}) => $noTopBar ? '' : 'max-content'} 1fr max-content;
 `;
