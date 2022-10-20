f80bff06383e138db109b8a696abe689974f4c10
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-10-20 09:51:09 +0200

    Adjusted props to ListItem comp

--- src/routers/Calendar/blocks/TimerList/TimerList.jsx
+++ src/routers/Calendar/blocks/TimerList/TimerList.jsx
@@ -1,19 +1,36 @@
+import { useContext } from 'react';
+import { UserDataContext } from '../../../../context/UserDataProvider';
 import { ListWrapper, ListItemWrapper } from './styled';
 
-const test = {
+/*
     color: 'red',
     title: 'Clean the office',
-    log: '00:00:00',
-    status: 'on going'
-}
-
+    start: Date
+    end: Date
+    log: true
+ */
 export default function TimerList() {
+    const { projects, tasks, logs } = useContext(UserDataContext);
+
+    if (logs.length === 0) return;
     return (
         <ListWrapper filled>
-            <ListItemWrapper values={test} />
-            <ListItemWrapper values={test} />
-            <ListItemWrapper values={test} />
-            <ListItemWrapper values={test} />
+            {
+                tasks.map(({ project_id, title, id }) => {
+                    const parentProject = projects.find(p => p.id === project_id);
+                    const { start, end } = logs.find(l => l.task_id === id);
+
+                    return <ListItemWrapper
+                        key={id}
+                        values={{
+                            color: parentProject.color,
+                            title: title,
+                            start: start,
+                            end: end,
+                            log: true
+                        }} />
+                })
+            }
         </ListWrapper>
     )
 }
