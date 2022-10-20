ea51c6485cc6552a22d06d3f6686c86817b17538
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-10-20 10:16:22 +0200

    Fix TopBar title

--- src/routers/Layout/TopBar/TopBar.jsx
+++ src/routers/Layout/TopBar/TopBar.jsx
@@ -1,13 +1,18 @@
+import { useMemo } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Wrapper } from './styled';
 
 export default function TopBar() {
     const { pathname } = useLocation();
-    // Adjust for calendar 
+    const pageTitle = useMemo(() => {
+        const pathList = pathname.split('/');
+        return pathList[pathList.length - 1]
+    }, [pathname]);
+
     if (pathname === '/') return;
     return (
         <Wrapper>
-            {pathname.slice(1)}
+            {pageTitle}
         </Wrapper>
     )
 }
