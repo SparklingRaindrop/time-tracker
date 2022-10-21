3ced04709ece88a6faebfbed2b97af91fc77dcb6
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-10-21 19:24:19 +0200

    Add function to select date range

--- src/routers/Calendar/Calendar.jsx
+++ src/routers/Calendar/Calendar.jsx
@@ -1,11 +1,26 @@
+import { useState } from 'react';
 import { GridCalendars, TimerList } from '../../blocks/Calendars';
 import { Main } from '../../components';
+import { compareDates } from '../../JS/date';
+
 
 export default function Calendar() {
+    const [duration, setDuration] = useState([new Date()]);
+
+    function updateDuration(newDate) {
+        setDuration(prev => {
+            const newDuration = [...prev, newDate];
+            newDuration.sort(compareDates);
+            if (newDuration.length < 3) return newDuration;
+            // When the old duration is selected reset on the third day clicked.
+            return [newDate];
+        });
+    }
+
     return (
         <Main>
-            <GridCalendars />
-            <TimerList />
+            <GridCalendars updateDuration={updateDuration} duration={duration} />
+            <TimerList duration={duration} />
         </Main>
     )
 }
