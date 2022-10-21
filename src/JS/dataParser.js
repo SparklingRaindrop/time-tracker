7213ed02d79831eb3c751317357748a2a98741e6
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-10-21 22:48:10 +0200

    Fix logic for getDayStatus

--- src/JS/dataParser.js
+++ src/JS/dataParser.js
@@ -1,3 +1,5 @@
+import { isValid } from './date';
+
 export function getStatus(isActive, end) {
         if (isActive) {
             return 'on going';
@@ -8,17 +10,27 @@ export function getStatus(isActive, end) {
         }
 }
 
-export function getDayStatus(date, duration) {
-    const convertedDate = Number(date);
-    if(!convertedDate) return;
+export function getDayStatus(targetDate, duration) {
+    if (!isValid(targetDate)) return;
+    const startDate = duration[0];
+    const endDate = duration[1];
 
-    const convertedDuration = duration.map(Number);
-    if (convertedDuration.includes(convertedDate)) {
+    // Resetting: Don't care about time, check just the day is within or not
+    startDate.setHours(0, 0, 0, 0);
+    targetDate.setHours(0, 0, 0, 0);
+    endDate && endDate.setHours(0, 0, 0, 0);
+
+    const [start, end, target] = [startDate, endDate, targetDate].map(date => ({
+        year: date && date.getFullYear(),
+        month: date && date.getMonth(),
+        day: date && date.getDate()
+    }));
+
+    if ((start.year === target.year && start.month === target.month && start.day === target.day) ||
+    (end.year === target.year && end.month === target.month && end.day === target.day)) {
         return 'selected';
-    } else if (
-        convertedDuration[0] < convertedDate &&
-        convertedDate < convertedDuration[1]
-    ) {
+    } else if ( (start.year <= target.year && start.month <= target.month && start.day < target.day) &&
+    (end.year >= target.year && end.month >= target.month && end.day > target.day)) {
         return 'within'
     }
     return 'normal';
