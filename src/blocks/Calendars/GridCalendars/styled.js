9a149bd991a21f798804730dd2105bc35090e911
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-10-21 22:48:52 +0200

    Separate calender element and add timePicker div

--- src/blocks/Calendars/GridCalendars/styled.js
+++ src/blocks/Calendars/GridCalendars/styled.js
@@ -1,13 +1,16 @@
 import styled from 'styled-components';
 import { Wrapper } from '../../../components';
 
-export const OuterWrapper = styled(Wrapper)`
+export const OuterWrapper = styled.div`
     // Lower half is 40%
     height: 60%;
+    padding: 0;
 
     display: flex;
     flex-direction: column;
     gap: 0.2rem;
+`;
 
+export const CalenderWrapper = styled(Wrapper)`
     overflow-y: auto;
 `;
