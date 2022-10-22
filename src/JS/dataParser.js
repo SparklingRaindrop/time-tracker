cb7ce8fdd868764977e2d4f16f83c26c1577c7bf
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-10-22 14:21:59 +0200

    Fix bug

--- src/JS/dataParser.js
+++ src/JS/dataParser.js
@@ -12,8 +12,8 @@ export function getStatus(isActive, end) {
 
 export function getDayStatus(targetDate, duration) {
     if (!isValid(targetDate)) return;
-    const startDate = duration[0];
-    const endDate = duration[1];
+    const startDate = new Date(duration[0]);
+    const endDate = new Date(duration[1]);
 
     // Resetting: Don't care about time, check just the day is within or not
     startDate.setHours(0, 0, 0, 0);
@@ -34,4 +34,11 @@ export function getDayStatus(targetDate, duration) {
         return 'within'
     }
     return 'normal';
+}
+
+export function getDoubleDigit(number) {
+    return number.toLocaleString('en-US', {
+        minimumIntegerDigits: 2,
+        useGrouping: false
+    })
 }
