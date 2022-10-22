7401094584b1e01ca6eb10100c7b2550813af5c6
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-10-22 15:24:52 +0200

    Added disabled status

--- src/components/IconButton/styled.js
+++ src/components/IconButton/styled.js
@@ -4,4 +4,8 @@ export const Button = styled.button`
     background: transparent;
     border: none;
     color: ${({theme}) => theme.colors.secondary};
+
+    &:disabled {
+        color: ${({theme}) => theme.colors.grey};
+    }
 `;
