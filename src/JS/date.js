317f37abc3eb82b4cb44dd2a66c2d694b7b30411
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-10-22 11:53:51 +0200

    Fix log filter function

--- src/JS/date.js
+++ src/JS/date.js
@@ -13,21 +13,23 @@ export function generateInitialDurationDate() {
 
 export function filterLogDataByDuration(duration, logArray) {
     if (!logArray.length) return [];
-
     const startDate = new Date(duration[0]);
-    const endDate = duration[1] || new Date(duration[1]);
+    const endDate = duration[1] ? new Date(duration[1]) : new Date(duration[0]);
+    // If duration has only one item then it's entire day
+    // If time for end date is 00:00:00, consider that as whole day
+    if(!duration[1] || (endDate.getHours() === 0 && endDate.getMinutes() === 0)) {
+        endDate.setHours(23, 59, 59);
+    }
+
     // Filter out logs whose start or end date is within duration
     return logArray.filter(({ start, end }) => {
-        const firstDate = new Date(start);
-        const secondDate = new Date(end);
+        const logStartDate = new Date(start);
+        const logEndDate = end && new Date(end);
 
-        return (startDate <= firstDate && firstDate <= endDate) ||
-        (startDate <= secondDate && secondDate <= endDate)
+        return (startDate <= logStartDate && logStartDate <= endDate) ||
+        (startDate <= logEndDate && logEndDate <= endDate)
     });
 }
-// TODO Things need to be adjusted later
-// Be careful with what Date instance have
-// it contains time nad if you don't care the time range you have to deal with that
 
 export function isValid(date) {
     return date instanceof Date && !isNaN(date);
