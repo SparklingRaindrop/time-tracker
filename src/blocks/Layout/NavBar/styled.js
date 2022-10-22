3cdc860ab52c6df5a49e78fbc37c8ebb3aa9a7c7
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-10-22 14:35:43 +0200

    Add fixed position on nav

--- src/blocks/Layout/NavBar/styled.js
+++ src/blocks/Layout/NavBar/styled.js
@@ -4,4 +4,6 @@ import {Nav} from '../../../components';
 export const Wrapper = styled(Nav)`
     grid-template-columns: repeat(3, 1fr);
     background: ${({theme}) => theme.colors.white};
+    position: fixed;
+    bottom: 0;
 `;
