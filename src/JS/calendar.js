a5eb42531810a7bff0313afaae1362c77d6c0644
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-10-22 14:21:50 +0200

    Fix logical bugs

--- src/JS/calendar.js
+++ src/JS/calendar.js
@@ -2,26 +2,73 @@
 
 import { compareDates } from './date';
 
+export function generateInitialDurationDate(date) {
+    const start = date ? new Date(date) : new Date();
+    const end = date ? new Date(date) :  new Date();
+    // I have to reset the time to get the initial day to be selected
+    // Because Day component doesn't have time value
+    // so it will be considered different when it's compared
+    start.setHours(0, 0, 0);
+    end.setHours(23, 59, 59);
+    return [start, end];
+}
+
 // value for time is {start '00:00', end '00:00' }
 export function durationReducer(state, { type, value }) {
-    if (type === 'date') {
-        const newDuration = [...state, value];
-        // Reset on the third day clicked.
-        if (newDuration.length === 3) return [value];
+    const newDuration = [...state];
 
-        newDuration.sort(compareDates);
-        return newDuration;
+    if (type === 'date') {
+        const start = new Date(state[0]);
+        const end = new Date(state[1]);
+        const newDate = new Date(value);
+
+        // Reset on the third day clicked.
+        const isRange = !(state[0].getFullYear() === state[1].getFullYear()) ||
+            !(state[0].getMonth() === state[1].getMonth()) ||
+            !(state[0].getDate() === state[1].getDate());
+        if (isRange) {
+            return generateInitialDurationDate(value);
+        }
+
+        let hour;
+        let minutes;
+        // If newDate is before start
+        if (compareDates(newDate, end) < 0) {
+            //take over time from start
+            hour = start.getHours();
+            minutes = start.getMinutes();
+            // Remove start
+            newDuration.splice(0, 1);
+        } else if (compareDates(newDate, end) > 0) {
+            // take over time from end
+            hour = end.getHours();
+            minutes = end.getMinutes();
+            // Remove end
+            newDuration.splice(0, 1);
+        } else {
+            console.error('Check!')
+        }
+        newDate.setHours(hour, minutes);
+        newDuration.push(newDate);
+        
+
 
     } else if (type === 'time') {
         const { start, end } = value;
-        const [startHour, startMinutes] = start.split(':');
-        const [endHour, endMinutes] = end.split(':');
-        const newStartDate = new Date(state[0]);
-        newStartDate.setHours(startHour, startMinutes);
-        const newEndDate = state.length === 1 ? new Date(state[0]) : new Date(state[1]);
-        newEndDate.setHours(endHour, endMinutes);
-        return [newStartDate, newEndDate];
+
+        const target = Object.keys(value)[0];
+        const targetIndex = target === 'start' ? 0 : 1;
+
+        const newStartDate = new Date(state[targetIndex]);
+        const [newHour, newMin] = start ? start : end;
+        newStartDate.setHours(newHour, newMin);
+
+        newDuration.splice(targetIndex, 1);
+        newDuration.push(newStartDate);
     }
+
+    newDuration.sort(compareDates);
+    return newDuration;
 }
 
     // const [duration, setDuration] = useState([generateInitialDurationDate()]);
