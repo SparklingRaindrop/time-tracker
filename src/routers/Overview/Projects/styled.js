a414c4066cf60ea28ec52e06522b571daaf33531
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-10-23 18:59:42 +0200

    Add overflow-y

--- src/routers/Overview/Projects/styled.js
+++ src/routers/Overview/Projects/styled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
-import { List } from '../../../components';
 
-export const ListWrapper = styled(List)`
-    // 4rem is TabLink's height
+export const ContentWrapper = styled.div`
     height: calc(100% - 4rem);
+    padding: 0 1rem;
+    overflow-y: auto;
 `;
