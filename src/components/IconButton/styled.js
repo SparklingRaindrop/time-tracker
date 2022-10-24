ee32af70e03ef57fa154160308c66aac5fed71cd
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-10-24 14:06:10 +0200

    Center Icon on Icon Button

--- src/components/IconButton/styled.js
+++ src/components/IconButton/styled.js
@@ -4,6 +4,9 @@ export const Button = styled.button`
     background: transparent;
     border: none;
     color: ${({theme}) => theme.colors.secondary};
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
     &:disabled {
         color: ${({theme}) => theme.colors.grey};
