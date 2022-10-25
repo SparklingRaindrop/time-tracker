efa0f100d43840f9a43fac0c080076438366746a
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-10-25 16:43:37 +0200

    Change background color

--- src/components/InputField/styled.js
+++ src/components/InputField/styled.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const InputField = styled.input`
     width: 100%;
     padding: 0.7em 1em;
-    background: ${({theme}) => theme.colors.primaryWhite};
+    background: ${({theme}) => theme.colors.white};
     border: none;
 
     &:placeholder {
