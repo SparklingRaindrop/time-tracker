204e8abc5004fd3035d956d678aa81f44c0f3e1e
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-10-25 09:42:40 +0200

    Add background color

--- src/components/InputField/styled.js
+++ src/components/InputField/styled.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 export const InputField = styled.input`
     width: 100%;
     padding: 0.7em 1em;
+    background: ${({theme}) => theme.colors.primaryWhite};
+    border: none;
 
     &:placeholder {
         text-transform: capitalize;
