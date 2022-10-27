0cef8b7c4de4250e79ac0a204b9d8e92f72c4763
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-10-27 17:08:16 +0200

    Add typography

--- src/components/GlobalStyles.jsx
+++ src/components/GlobalStyles.jsx
@@ -13,8 +13,66 @@ export const GlobalStyles = createGlobalStyle`
         overflow-x: hidden;
     }
 
+
+    // Typography
+    body,
+    input,
+    button,
+    h3,
+    h4,
+    h5,
+    h6 {
+        font-family: 'Poppins', sans-serif;
+    }
+
+    h3,
+    h4,
+    h5,
+    h6 {
+        font-weight: 600;
+    }
+    
+    h3 {
+        font-size: 1.9rem;
+    }
+
+    h4 {
+        font-size: 1.5rem;
+    }
+
+    h5 {
+        font-size: 1.25rem;
+    }
+
+    h6 {
+        font-size: 1rem;
+    }
+
+    body,
     input,
     button {
-        font-size: 1.2rem;
+        font-size: 1.2em;
+        font-weight: 400;
     }
+
+    h1,
+    h2 {
+        font-family: 'Righteous', cursive;
+        font-weight: 400;
+    }
+
+    h1 {
+        font-size: 3rem;
+        text-align: center;
+    }
+
+    h2 {
+        font-size: 2.4rem;
+    }
+
+    button,
+    label {
+        text-transform: capitalize;
+    }
+
 `;
